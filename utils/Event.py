50f048dce861925851a91d037e2f17f67cefad34
Roman <strobl17@seznam.cz>
2022-05-15 17:48:09 +0200

    update

--- utils/Event.py
+++ utils/Event.py
@@ -2,16 +2,16 @@ subscribers = dict()
 
 
 def subscribe(event: str, fn):
-    if not event in subscribers:
+    if event not in subscribers:
         subscribers[event] = []
     subscribers[event].append(fn)
 
-def fire_event(event: str, data):
-    if not event in subscribers:
+
+def fire_event(event: str, data=None):
+    if event not in subscribers:
         return
     for fn in subscribers[event]:
         if data is None:
             fn()
         else:
             fn(data)
-
