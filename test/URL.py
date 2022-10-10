c5be54480478efce073948d8f64e659b97bcdf3e
Roman <strobl17@seznam.cz>
2022-10-10 15:28:11 +0200

    Delete URL.py

--- test/URL.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-
-url = "http://127.0.0.1:5000/printer/queue"
-parsed_url = urllib.parse.urlparse(url)
-
-print(parsed_url)
-print("Host name: ", parsed_url.netloc)
-print(parsed_url.scheme)
